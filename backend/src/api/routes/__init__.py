a613388f864c0e9df7e7beff8754c33242304b60
Karl Justine B. Catahan <karljustinecatahan@gmail.com>
2026-08-26 21:05:40 +0800

    chore(api): register audit and organization routers

--- backend/src/api/routes/__init__.py
+++ backend/src/api/routes/__init__.py
@@ -3,8 +3,10 @@
 from fastapi import APIRouter
 
 from src.api.routes.attestation_routes import router as attestation_router
+from src.api.routes.audit_routes import router as audit_router
 from src.api.routes.auth_routes import router as auth_router
 from src.api.routes.health import router as health_router
+from src.api.routes.organization_routes import router as organization_router
 from src.api.routes.scan_routes import router as scan_router
 from src.api.routes.target_routes import router as target_router
 from src.config.constants import API_V1_STR
@@ -15,5 +17,7 @@ api_router.include_router(auth_router)
 api_router.include_router(target_router)
 api_router.include_router(attestation_router)
 api_router.include_router(scan_router)
+api_router.include_router(organization_router)
+api_router.include_router(audit_router)
 
 __all__ = ["api_router", "health_router"]
